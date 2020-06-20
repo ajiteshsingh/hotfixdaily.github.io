af42e3fe7287ebdf5bd5c6157b1a4f0f0dca1633
ajiteshsingh <ajitesh@invideo.io>
2020-06-20 16:25:08 +0530

    browser extension

--- src/app/services/post-page.service.ts
+++ src/app/services/post-page.service.ts
@@ -1,25 +1,29 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class PostPageService {
-  constructor() {}
+  constructor(
+    // private http: HttpClient
+  ) { }
 
   getPost(url: string) {
     return new Promise((resolve, reject) => {
-      // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      // const proxyurl = 'https://cors-anywhere.herokuapp.com/';
       fetch(url, {
-        mode: "cors",
-        headers: {
-          "Access-Control-Allow-Origin": "*"
-        }
+        // mode: 'cors',
+        // headers: {
+        //   'Access-Control-Allow-Origin': '*',
+        // }
       })
         .then(res => res.json())
         .then(response => {
           resolve(response);
         })
         .catch(err => {
+          alert('A browser extension is blocking us, please open this url in incognito');
           reject();
         });
     });
